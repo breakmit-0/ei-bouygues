950e93a60d650043a1edea29aab025107fb1412d
breakmit-0 <bonbaoke@@gmail.com>
2024-01-30 08:58:48 +0100

    estimation

--- estimation.py
+++ estimation.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
+import time
+import datetime
+
+def ts(s):
+    return time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())
 
 data = pd.read_csv("clean.csv", delimiter=",", skiprows=0)
 data = data[["tstamp", "trafic_mbps"]]
@@ -20,9 +25,17 @@ t = _t
 res = stats.linregress(t, y)
 
 
+def modele(x):
+    return res.slope * x + res.intercept
+
+
 print("rvalue =", res.rvalue)
 print(f"equation : y = {res.slope} * t + {res.intercept}")
+print("prediction 2030 =", round(modele(ts("01/01/2030"))), "mbps")
+print("prediction 2024 =", round(modele(ts("01/01/2024"))), "mbps")
 
 plt.plot(t,y)
-plt.plot(t, res.slope * t + res.intercept)
+plt.plot(t, modele(t))
 plt.show()
+
+
