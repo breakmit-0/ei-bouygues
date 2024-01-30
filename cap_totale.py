a2abdf5ce741b7ce910b9c7c5a5a01351eff727a
breakmit-0 <bonbaoke@@gmail.com>
2024-01-30 09:45:45 +0100

    cap totale

--- cap_totale.py
+++ cap_totale.py
@@ -3,11 +3,28 @@
 import pandas as pd
 from abaque import efficacite
 
-data = pd.read_csv("secteurs.csv")
+data = pd.read_csv("secteurs_clean.csv")
 
 largeurs = [5, 10, 20, 15, 15, 70]
-largeurs = {"700": 4, "800": 9, "1800": 19, "2100": 14, "2600": 14, "3500": 69]
+largeurs = {"700": 4, "800": 9, "1800": 19, "2100": 14, "2600": 14, "3500": 69}
+modes = {"700": "4g", "800": "4g", "1800": "4g", "2100": "4g", "2600": "4g", "3500": "5g"}
 bandes = ["700", "800", "1800", "2100", "2600", "3500"]
 
+total = 0
 
-print(data)
+for b in bandes:
+    header = f"{b} MHz"
+
+    count = data[header].map(lambda x: int(x)).sum()
+    
+    mul = 1
+    if modes[b] == "5g":
+        mul = 1.2
+    
+    local = efficacite * largeurs[b] * mul * count
+    total += local 
+    print(f"bande {b} MHz : {local} mbps (count = {count})")
+
+
+
+print(f"total = {total}")
--- clean_secteurs.py
+++ clean_secteurs.py
@@ -7,6 +7,9 @@ data = pd.read_csv("secteurs.csv")
 def bool_remap(x):
     return x == "oui"
 
-data["700 MHz"].map(bool_remap)
+bandes = ["700 MHz", "800 MHz", "1800 MHz", "2100 MHz", "2600 MHz", "3500 MHz"]
+
+for b in bandes:
+    data[b] = data[b].map(bool_remap)
 
 data.to_csv("secteurs_clean.csv")
